533968494c9e39e74caed70ba099cdec8d1ba1f7
Gavin <racinggavin@gmail.com>
2024-03-07 01:49:29 +0800

    Schema modify, add Response.aimTo property

--- src/prisma/createOneResponse.py
+++ src/prisma/createOneResponse.py
@@ -4,7 +4,10 @@ from response import Response
 from user import User
 from util import glog
 
-async def create_one_response_core(db: Prisma, response: Response) -> Response:
+async def create_one_response_core(db: Prisma, response: Response, aimTo: Mood) -> Response:
+	aim_to_mood_timestamp = None
+	if aimTo != None:
+		aim_to_mood_timestamp = aimTo.timestamp
 	responseDb = await db.response.create(
 		data = {
 			'user': {
@@ -14,6 +17,7 @@ async def create_one_response_core(db: Prisma, response: Response) -> Response:
 			},
 			'group_id': response.group_id,
 			'ai_text': response.ai_text,
+			'aim_to_mood_timestamp': aim_to_mood_timestamp,
 			'lastUser': {
 				'connect': {
 					'user_id': response.user_id
@@ -23,15 +27,15 @@ async def create_one_response_core(db: Prisma, response: Response) -> Response:
 	)
 	return responseDb
 
-async def create_one_response(response: Response) -> Response:
+async def create_one_response(response: Response, aimTo: Mood) -> Response:
 	async with Prisma() as db:
-		return await create_one_response_core(db, response)
+		return await create_one_response_core(db, response, aimTo)
 
-async def main() -> None:
-	newResponse = Response(user_id='2234',group_id=None,ai_text='第一個回應來嘍')
-	ans = await create_one_response(newResponse)
-	if ans != None:
-		glog(f' => {ans}')
+# async def main() -> None:
+# 	newResponse = Response(user_id='2234',group_id=None,ai_text='第一個回應來嘍')
+# 	ans = await create_one_response(newResponse)
+# 	if ans != None:
+# 		glog(f' => {ans}')
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# if __name__ == '__main__':
+#     asyncio.run(main())
--- src/prisma/response.py
+++ src/prisma/response.py
@@ -6,4 +6,5 @@ class Response:
 	user_id: str = None
 	group_id: str = None
 	ai_text: str
+	aim_to_mood_timestamp: str = None
 	timestamp: str = field(init=False, default_factory=nowTimeStampISO)
