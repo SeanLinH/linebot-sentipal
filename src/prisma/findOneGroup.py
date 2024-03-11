d865800f5c3932914c41c26f9fc6646f9bcd118b
Gavin <racinggavin@gmail.com>
2024-03-11 09:46:00 +0800

    Fix bug Group.belongToUserId has been removed

--- src/prisma/findOneGroup.py
+++ src/prisma/findOneGroup.py
@@ -1,5 +1,6 @@
 import asyncio
 from prisma import Prisma
+from src.prisma.user import User
 from src.prisma.group import Group
 from src.prisma.util import glog
 
@@ -7,24 +8,39 @@ from src.prisma.util import glog
 find group_id in [Groups] or not
 """
 
-async def find_one_group_core(db: Prisma, groupId: str) -> Group:
+async def find_one_group_core(db: Prisma, groupId: str, includeGroupUsers: bool) -> Group:
 	bingo = await db.group.find_unique(
 			where={
 				'group_id': groupId
+			},
+			include={
+				'groupUsers': includeGroupUsers,
 			}
 		)
 	# glog(f'bingo => {bingo}')
 	if bingo == None:
 		return None
+	
+	if includeGroupUsers:
+		groupUsers = []
+		for e in bingo.groupUsers:
+			groupUsers.append(User(
+				user_id=e.user_id, 
+				user_role=e.user_role,
+				summary=e.summary,
+				lastmood_timestamp=e.lastmood_timestamp,
+				lastresponse_timestamp=e.lastresponse_timestamp
+			))
+
 	return Group(
 		group_id    	= bingo.group_id,
-		belongToUserId  = bingo.belongToUserId,
+		groupUsers 		= groupUsers,
 		belongToUserApiId = bingo.belongToUserApiId,
 		)
 
-async def find_one_group(groupId: str) -> Group:
+async def find_one_group(groupId: str, includeGroupUsers: bool=False) -> Group:
 	async with Prisma() as db:
-		return await find_one_group_core(db, groupId)
+		return await find_one_group_core(db, groupId, includeGroupUsers)
 
 # async def main() -> None:
 # 	ans = await find_one_group('')
